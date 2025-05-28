51782696ccc28b06f47e4cf57c4e8f4546c9fc3c
TUSHAR <tushar9445958996@gmail.com>
2025-05-28 20:13:07 +0530

    Added gradient

---
+++ components/main/Hero.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Hero() {
+  return (
+    <div className="relative flex flex-col h-full w-full">
+      <video
+        autoPlay
+        loop
+        muted
+        className="rotate-180 absolute top-[-340px] left-0 z-[-1] w-full h-full object-cover"
+      >
+        <source
+            src='/blackhole.webm'
+            type='video/webm'
+        />
+      </video>
+    </div>
+  );
+}
