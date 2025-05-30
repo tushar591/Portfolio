3611eb3600e5ab09b6c915550b68f51ea75fbc91
TUSHAR <tushar9445958996@gmail.com>
2025-05-30 20:23:49 +0530

    Added Navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import StarsCanvas from "../../components/main/StarBackground";
+import Navbar from "../../components/main/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#030014] overflow-y-scroll overflow-x-hidden`}
       >
         <StarsCanvas />
+        <Navbar/>
         {children}
       </body>
     </html>
