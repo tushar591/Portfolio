29cd1851b0d0df37e1139ecfcfb211e058747ef2
TUSHAR <tushar9445958996@gmail.com>
2025-06-05 16:52:00 +0530

    Edited ESlint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Hero from '../../components/main/Hero';
 import Skills from '../../components/main/Skill';
 import Projects from '../../components/main/Projects';
-import Footer from '../../components/main/Footer';
 
 export default function Home() {
   return (
