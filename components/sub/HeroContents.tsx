9ca7ff9d79caaebdacafb810c43ed7a314760b21
TUSHAR <tushar9445958996@gmail.com>
2025-06-06 17:16:00 +0530

    Added Rotating text

--- components/sub/HeroContents.tsx
+++ components/sub/HeroContents.tsx
@@ -1,12 +1,12 @@
-"use client"
+"use client";
 import React from "react";
 import { motion } from "framer-motion";
-import { slideInFromLeft, slideInFromRight, slideInFromTop } from "../../utils/motion";
-import { SparklesIcon } from "@heroicons/react/24/outline";
+import { slideInFromLeft, slideInFromRight } from "../../utils/motion";
 import Image from "next/image";
+import RotatingText from "./RotatingText";
 
 export default function HeroContents() {
- return (
+  return (
     <motion.div
       initial="hidden"
       animate="visible"
@@ -14,26 +14,27 @@ export default function HeroContents() {
     >
       <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
         <motion.div
-          variants={slideInFromTop}
-          className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
-        >
-          <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
-          <h1 className="Welcome-text text-[13px]">
-            Fullstack Developer Portfolio
-          </h1>
-        </motion.div>
-
-        <motion.div
           variants={slideInFromLeft(0.5)}
           className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
         >
           <span>
-            Providing
+            Hi, I&apos;m{" "}
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
               {" "}
-              the best{" "}
-            </span>
-            project experience
+              Tushar Bharat{" "}
+            </span> 
+            <RotatingText
+              texts={["Web Developer", "Coder!", "UI/UX Designer"]}
+              mainClassName="px-2 sm:px-2 md:px-2 bg-cyan-500 text-black overflow-hidden py-1 sm:py-1 md:py-2 justify-left rounded-lg"
+              staggerFrom={"last"}
+              initial={{ y: "100%" }}
+              animate={{ y: 0 }}
+              exit={{ y: "-120%" }}
+              staggerDuration={0.025}
+              splitLevelClassName="overflow-hidden pb-1 sm:pb-2 md:pb-1"
+              transition={{ type: "spring", damping: 30, stiffness: 400 }}
+              rotationInterval={2000}
+            />
           </span>
         </motion.div>
 
@@ -41,8 +42,11 @@ export default function HeroContents() {
           variants={slideInFromLeft(0.8)}
           className="text-lg text-gray-400 my-5 max-w-[600px]"
         >
-          I&apos;m a Full Stack Software Engineer with experience in Website,
-          DSA and Software development. Check out my projects and skills.
+          I am a full-stack developer with a passion for creating innovative
+          solutions in building scalable web applications. Skilled in both
+          front-end and back-end development, I specialize in the MERN stack and
+          other modern technologies to create seamless user experiences and
+          efficient solutions.
         </motion.p>
         <motion.a
           variants={slideInFromLeft(1)}
