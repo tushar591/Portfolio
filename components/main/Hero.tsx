28a8fa8c439e70241eecdfb6448cd49390ed6a40
TUSHAR <tushar9445958996@gmail.com>
2025-07-19 23:35:08 +0530

    Added some responsive features

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -3,7 +3,7 @@ import HeroContents from "../sub/HeroContents";
 
 export default function Hero() {
   return (
-    <div id="about-me" className="relative flex flex-col h-full w-full">
+    <div className="relative flex flex-col h-full w-full">
      
       <HeroContents />
     </div>
